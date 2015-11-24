d9598cd6f24a99f132b6765f1611904fb8799d91
dongreenberg <dongreenberg2@gmail.com>
2015-11-23 23:31:55 -0500

    Create main.m

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NoLiePushupCounter
+//
+//  Created by Don Greenberg Greenberg on 12/31/11.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
